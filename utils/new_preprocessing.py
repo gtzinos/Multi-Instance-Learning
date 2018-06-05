16addec648b0d00a365fa8f68c0c0f06338dd040
George Tzinos <geotzinos@gmail.com>
2018-06-05 11:16:18 +0300

    FIX Preprocess

--- utils/new_preprocessing.py
+++ utils/new_preprocessing.py
@@ -2,22 +2,10 @@ import re
 import csv
 
 
-def export_binary_labels(most_frequent_index, outfp, lineString):
-    columns = lineString.split(',')
-
-    for index, column in enumerate(columns):
-        if index == (most_frequent_index + 1):
-            if column == "1":
-                outfp.write("1,-1\n")
-            else:
-                outfp.write("-1,1\n")
-
-
 def convert_labels_to_binary(input_file_path, most_frequent_index, output_file_path):
     fp = open(input_file_path, 'r')
     outfp = open(output_file_path, 'w')
 
-    ln = fp.readline()
     outfp.write("reference,other\n")
 
     while True:
@@ -25,7 +13,14 @@ def convert_labels_to_binary(input_file_path, most_frequent_index, output_file_p
         if len(ln) == 0:
             break
 
-        export_binary_labels(most_frequent_index, fp, ln)
+        columns = ln.split(' ')
+
+        for index, column in enumerate(columns):
+            if index == (most_frequent_index + 1):
+                if column == "1":
+                    outfp.write("1 -1\n")
+                else:
+                    outfp.write("-1 1\n")
 
     outfp.close()
     fp.close()
@@ -48,7 +43,7 @@ def convert_sentences_to_binary(input_file_path, most_frequent_index, output_fil
             columns = part.split(' ')
 
             for index, column in enumerate(columns):
-                #print(part)
+                # print(part)
                 if index == (most_frequent_index + 1):
                     if column == "1":
                         outfp.write("<1 -1>")
@@ -61,5 +56,11 @@ def convert_sentences_to_binary(input_file_path, most_frequent_index, output_fil
     fp.close()
 
 
-#convert_labels_to_binary("../data/train-label", 2, "./test-results")
-convert_sentences_to_binary("../data/test-sentlabel.dat", 2, "./test-results")
+convert_labels_to_binary("../data/train-label.dat", 2,
+                         "../data/new_train-label.dat")
+convert_labels_to_binary("../data/test-label.dat", 2,
+                         "../data/new_test-label.dat")
+
+#convert_sentences_to_binary("../data/train-sentlabel.dat", 2, "../data/new_train-sentlabel.dat")
+convert_sentences_to_binary(
+    "../data/test-sentlabel.dat", 2, "../data/new_test-sentlabel.dat")
